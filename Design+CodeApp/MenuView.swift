cff1d8d388e8f49e7eb1ba4f91aec5c0c9e31ba2
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-12 13:37:50 -0500

    Initial Commit

---
+++ Design+CodeApp/MenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuView.swift
+//  Design+CodeApp
+//
+//  Created by Matthew Garlington on 2/12/21.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
