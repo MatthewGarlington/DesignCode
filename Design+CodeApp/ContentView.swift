7c37b6f2da174aa34cb3382428bbbfb2f43e566c
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-12 08:51:38 -0500

    Initial Commit

---
+++ Design+CodeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Design+CodeApp
+//
+//  Created by Matthew Garlington on 2/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Design+CodeApp/Design_CodeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_CodeAppApp.swift
+//  Design+CodeApp
+//
+//  Created by Matthew Garlington on 2/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Design_CodeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
