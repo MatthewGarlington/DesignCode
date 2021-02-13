43948d27081572638f72f04bcc49ef9e20fccf74
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-13 15:49:32 -0500

    Init Commit

---
+++ Design+CodeApp/CourseList.swift
@@ -0,0 +1,20 @@
+//
+//  CourseList.swift
+//  Design+CodeApp
+//
+//  Created by Matthew Garlington on 2/13/21.
+//
+
+import SwiftUI
+
+struct CourseList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourseList_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseList()
+    }
+}
