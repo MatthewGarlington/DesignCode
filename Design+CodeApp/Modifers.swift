195d854cc82aaae234c12ad879ab5e44cc6c6b70
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-13 14:47:14 -0500

    Init Commit/ Fonts Added

---
+++ Design+CodeApp/Modifers.swift
@@ -0,0 +1,20 @@
+//
+//  Modifers.swift
+//  Design+CodeApp
+//
+//  Created by Matthew Garlington on 2/13/21.
+//
+
+import SwiftUI
+
+struct Modifers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Modifers_Previews: PreviewProvider {
+    static var previews: some View {
+        Modifers()
+    }
+}
