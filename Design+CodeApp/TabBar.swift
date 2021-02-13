a5c131660b3dae65cb5a594bf42fd46ab0e902bb
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-13 07:36:48 -0500

    Feat: Added TabBar View with two sections: Home and ContentView

--- Design+CodeApp/TabBar.swift
+++ Design+CodeApp/TabBar.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct TabBar: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            Home().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("Home")
+            }
+            ContentView().tabItem {
+                Image(systemName: "rectangle.stack.fill")
+                Text("Certificates")
+                
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
