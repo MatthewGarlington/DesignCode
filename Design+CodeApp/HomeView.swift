77670c5d09492f5639bdba804b01a510429960b0
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-13 14:50:21 -0500

    Feat: Added Custom Shadow Modifer and Custom Font Modifier
    Added to the HomeView were to modifiers for drop shadow to be reused. The Progress view at the top was placed in a horizontal scroll view and two more progress views were placed in with static progress and custom colors for each

--- Design+CodeApp/HomeView.swift
+++ Design+CodeApp/HomeView.swift
@@ -15,7 +15,8 @@ struct HomeView: View {
         VStack {
             HStack {
                 Text("Watching")
-                    .font(.system(size: 28, weight: .bold))
+                  .font(.system(size: 28, weight: .bold))
+                 
                 
                 Spacer()
                 
@@ -40,24 +41,11 @@ struct HomeView: View {
             .padding(.leading, 14)
             .padding(.top, 30)
             
-            HStack(spacing: 12) {
-                RingView(color1: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), width: 44, height: 44, percent: 68, show: .constant(true))
-                
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("6 minutes left")
-                        .font(.subheadline)
-                        .fontWeight(.bold)
-                    Text("Watched 10 minutes today")
-                        .font(.caption)
-                }
-                
+            ScrollView(.horizontal, showsIndicators: false) {
+                WatchRingsView()
+                    .padding(.horizontal, 30)
+                    .padding(.bottom, 30)
             }
-                
-                    .padding(8)
-                    .background(Color.white)
-                    .cornerRadius(20)
-                    .shadow(color: .black.opacity(0.2), radius: 20, x: 0, y: 20)
-                    .shadow(color: .black.opacity(0.1), radius: 1, x: 0, y: 1)
             
             
             ScrollView(.horizontal, showsIndicators: false) {
@@ -140,3 +128,59 @@ let sectionData = [
     Section(title: "SwiftUI Advanced", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card3")), color: Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))),
     
 ]
+
+struct WatchRingsView: View {
+    var body: some View {
+        HStack(spacing: 30) {
+            HStack(spacing: 12) {
+                RingView(color1: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), width: 44, height: 44, percent: 68, show: .constant(true))
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("6 minutes left").modifier(FontModifer(style: .subheadline))
+                    Text("Watched 10 minutes today")
+                        .modifier(FontModifer(style: .caption))
+                    
+                }
+                .modifier(FontModifer())
+                
+            }
+            
+            
+            
+            .padding(8)
+            .background(Color.white)
+            .cornerRadius(20)
+            .modifier(ShadowModifer())
+            
+            HStack {
+                HStack(spacing: 12) {
+                    RingView(color1: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), color2: #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), width: 32, height: 32, percent: 54, show: .constant(true))
+                        
+                        
+                        .modifier(FontModifer())
+                    
+                }
+                
+                .padding(8)
+                .background(Color.white)
+                .cornerRadius(20)
+                .modifier(ShadowModifer())
+            }
+            
+            HStack {
+                HStack(spacing: 12) {
+                    RingView(color1: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), width: 32, height: 32, percent: 32, show: .constant(true))
+                        
+                        
+                        .modifier(FontModifer())
+                    
+                }
+                
+                .padding(8)
+                .background(Color.white)
+                .cornerRadius(20)
+                .modifier(ShadowModifer())
+            }
+        }
+    }
+}
