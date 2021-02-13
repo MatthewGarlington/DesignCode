97496b820f2dc5e3a334c6107615d1ae94617323
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-12 19:40:57 -0500

    Init Commit

---
+++ Design+CodeApp/UpdateDetail.swift
@@ -0,0 +1,20 @@
+//
+//  UpdateDetail.swift
+//  Design+CodeApp
+//
+//  Created by Matthew Garlington on 2/12/21.
+//
+
+import SwiftUI
+
+struct UpdateDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UpdateDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateDetail()
+    }
+}
