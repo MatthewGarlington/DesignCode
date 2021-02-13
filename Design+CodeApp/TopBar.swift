4af57593f80d575e83ac1774df69ab14cd3ea224
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-13 07:35:20 -0500

    Init commit

---
+++ Design+CodeApp/TopBar.swift
@@ -0,0 +1,20 @@
+//
+//  TopBar.swift
+//  Design+CodeApp
+//
+//  Created by Matthew Garlington on 2/13/21.
+//
+
+import SwiftUI
+
+struct TopBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TopBar()
+    }
+}
