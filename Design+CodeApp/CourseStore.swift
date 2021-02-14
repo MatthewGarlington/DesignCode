e86fc90d7d1d75b8d88ae1290668f6fc86085cac
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-14 13:44:32 -0500

    Prototyping: Set Course List to Launch on start of app to preview Contentful API info in Debugging console

---
+++ Design+CodeApp/CourseStore.swift
@@ -0,0 +1,20 @@
+//
+//  CourseStore.swift
+//  Design+CodeApp
+//
+//  Created by Matthew Garlington on 2/14/21.
+//
+
+import SwiftUI
+
+struct CourseStore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourseStore_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseStore()
+    }
+}
