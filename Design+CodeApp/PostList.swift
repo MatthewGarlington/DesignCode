572861ff546139af910a34b07e2299f783fdd3d7
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-14 12:59:53 -0500

    Feat: Added Observed Object from the combine DataStore
    Added the ability for the List view to present the title and body of the data from the api call through the combine DataStore file

--- Design+CodeApp/PostList.swift
+++ Design+CodeApp/PostList.swift
@@ -9,31 +9,40 @@ import SwiftUI
 
 struct PostList: View {
     
-    @State var posts: [Post] = []
+    @ObservedObject var store = DataStore()
  
     
     var body: some View {
-        // Adding the list with a number of items that the API gives us
-        List(posts) { post in
-            Text(post.title)
-      
+        // Adding the list with a number of items that the API gives us using combine to represent data through the DataStore 
+        List(store.posts) { post in
+            VStack(alignment: .leading, spacing: 8) {
+                Text(post.title).font(.system(.title, design: .serif)).bold()
+                
+                Text(post.body).font(.subheadline).foregroundColor(.secondary)
+            }
+  
             }
         
-        .onAppear {
-            
-            // Adding the posts array and the State var allows us to manipulate the data given to us from the API
 
-            Api().getPosts { (posts) in
-                
-                // Adding the posts to the State posts
-                self.posts = posts
-            }
         }
     }
-}
+
 
 struct PostList_Previews: PreviewProvider {
     static var previews: some View {
         PostList()
     }
 }
+
+
+//// This Adds the ability to view the API Data before adding the combine method
+//.onAppear {
+//
+//    // Adding the posts array and the State var allows us to manipulate the data given to us from the API
+//
+//    Api().getPosts { (posts) in
+//
+//        // Adding the posts to the State posts
+//        self.posts = posts
+//    }
+// }
