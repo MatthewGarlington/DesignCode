7076d0ce940e4a56a25403b03b5ae6577240463b
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-14 11:35:08 -0500

    Init commit

---
+++ Design+CodeApp/PostList.swift
@@ -0,0 +1,20 @@
+//
+//  PostList.swift
+//  Design+CodeApp
+//
+//  Created by Matthew Garlington on 2/14/21.
+//
+
+import SwiftUI
+
+struct PostList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostList_Previews: PreviewProvider {
+    static var previews: some View {
+        PostList()
+    }
+}
