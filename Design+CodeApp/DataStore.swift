4c138b2f3b9fab7a4c532dcae55ba5532e1e05a3
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-14 12:46:25 -0500

    Init Commit

---
+++ Design+CodeApp/DataStore.swift
@@ -0,0 +1,20 @@
+//
+//  DataStore.swift
+//  Design+CodeApp
+//
+//  Created by Matthew Garlington on 2/14/21.
+//
+
+import SwiftUI
+
+struct DataStore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DataStore_Previews: PreviewProvider {
+    static var previews: some View {
+        DataStore()
+    }
+}
