69875f54bc3659ae8d03734c37e650b737d722ec
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-14 11:41:07 -0500

    Feat: Added PostList file to present list of title from the JSON Placeholder API

--- Design+CodeApp/PostList.swift
+++ Design+CodeApp/PostList.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct PostList: View {
+    
+    @State var posts: [Post] = []
+ 
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // Adding the list with a number of items that the API gives us
+        List(posts) { post in
+            Text(post.title)
+      
+            }
+        
+        .onAppear {
+            
+            // Adding the posts array and the State var allows us to manipulate the data given to us from the API
+
+            Api().getPosts { (posts) in
+                
+                // Adding the posts to the State posts
+                self.posts = posts
+            }
+        }
     }
 }
 
