acccf38725a6e7af450d4fcdaeb10988d2b93f34
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-15 09:11:33 -0500

    Prototyping: Seperating Keys

---
+++ Design+CodeApp/Keyd.swift
@@ -0,0 +1,20 @@
+//
+//  Keyd.swift
+//  Design+CodeApp
+//
+//  Created by Matthew Garlington on 2/15/21.
+//
+
+import SwiftUI
+
+struct Keyd: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Keyd_Previews: PreviewProvider {
+    static var previews: some View {
+        Keyd()
+    }
+}
---
+++ Design+CodeApp/Keys.swift
@@ -0,0 +1,20 @@
+//
+//  Keys.swift
+//  Design+CodeApp
+//
+//  Created by Matthew Garlington on 2/15/21.
+//
+
+import SwiftUI
+
+struct Keys: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Keys_Previews: PreviewProvider {
+    static var previews: some View {
+        Keys()
+    }
+}
