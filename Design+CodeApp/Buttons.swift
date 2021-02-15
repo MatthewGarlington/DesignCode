6e17b36c226aaefa7bc6a0cc4aa7986d19349be5
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-15 12:18:26 -0500

    Init Commit

---
+++ Design+CodeApp/Buttons.swift
@@ -0,0 +1,20 @@
+//
+//  Buttons.swift
+//  Design+CodeApp
+//
+//  Created by Matthew Garlington on 2/15/21.
+//
+
+import SwiftUI
+
+struct Buttons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Buttons_Previews: PreviewProvider {
+    static var previews: some View {
+        Buttons()
+    }
+}
