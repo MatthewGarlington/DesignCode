a4ae7117204b0a16f6d1de7d0b01fce1157d55b3
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-16 14:05:18 -0500

    feat: Imported and intitalized firebase to be used throughout Firebase application

--- Design+CodeApp/Design_CodeAppApp.swift
+++ Design+CodeApp/Design_CodeAppApp.swift
@@ -6,12 +6,33 @@
 //
 
 import SwiftUI
+import Firebase
+
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    print("Colors application is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
+    return true
+  }
+}
+
 
 @main
+
 struct Design_CodeAppApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+      FirebaseApp.configure()
+    }
+   
     var body: some Scene {
         WindowGroup {
-            LoadingView()
+            LoginView()
+            
         }
     }
 }
