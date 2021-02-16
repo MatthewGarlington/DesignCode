5aa01ff9dedcd2273932604951e9b63179bcfb9c
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-16 17:53:31 -0500

    feat: Added User Defualts for Persitant State of logged in

--- Design+CodeApp/UserStore.swift
+++ Design+CodeApp/UserStore.swift
@@ -9,7 +9,14 @@ import SwiftUI
 import Combine
 
 class UserStore: ObservableObject {
-    @Published var isLogged = false
+    // Setting the User Defaults in order to save Data after user closes the app to prevent having to login every time
+    @Published var isLogged: Bool = UserDefaults.standard.bool(forKey: "isLogged") {
+        didSet {
+            UserDefaults.standard.set(self.isLogged, forKey: "isLogged")
+            
+        }
+        
+    }
     @Published var showLogin = false 
     
 }
