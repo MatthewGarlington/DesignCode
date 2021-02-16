42d82609ba61ef293adc4ec29eeb895a919a172b
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-16 10:43:51 -0500

    init Commit

---
+++ Design+CodeApp/SuccessView.swift
@@ -0,0 +1,20 @@
+//
+//  SuccessView.swift
+//  Design+CodeApp
+//
+//  Created by Matthew Garlington on 2/16/21.
+//
+
+import SwiftUI
+
+struct SuccessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SuccessView_Previews: PreviewProvider {
+    static var previews: some View {
+        SuccessView()
+    }
+}
