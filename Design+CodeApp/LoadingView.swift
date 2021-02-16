af2bc19f794bb2e06ea0178d53db6ecad420cbd5
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-16 10:01:16 -0500

    Init commit

---
+++ Design+CodeApp/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  Design+CodeApp
+//
+//  Created by Matthew Garlington on 2/16/21.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
