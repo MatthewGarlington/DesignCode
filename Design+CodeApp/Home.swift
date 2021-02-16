cd65beff37237e0c44f2e9e6a95201b7580f691d
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-16 15:35:07 -0500

    feat: Added Envirmental objects to home in order for the User to be able to Login through the information entered, and if it is correct proceed

--- Design+CodeApp/Home.swift
+++ Design+CodeApp/Home.swift
@@ -11,6 +11,7 @@ struct Home: View {
     @State var showProfile = false
     @State var viewState = CGSize.zero
     @State var showContent = false
+    @EnvironmentObject var user: UserStore
     
     var body: some View {
         
@@ -66,6 +67,31 @@ struct Home: View {
                     
                 )
             
+            // Direct the user to the login screen if a profile is not logged into
+            if user.showLogin {
+                
+                ZStack {
+                    LoginView()
+                    
+                    VStack {
+                        HStack {
+                            Spacer()
+                          
+                            Image(systemName: "xmark")
+                                .frame(width: 36, height: 36)
+                                .foregroundColor(.white)
+                                .background(Color.black)
+                                .clipShape(Circle())
+                        }
+                        Spacer()
+                    }
+                    .padding()
+                    .onTapGesture {
+                        self.user.showLogin = false 
+                    }
+                }
+            }
+            
             if showContent {
                 
                 BlurView(style: .systemMaterial)
@@ -101,19 +127,38 @@ struct Home: View {
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
-        Home().environment(\.colorScheme, .dark)
+        Home()
+            .environmentObject(UserStore())
     }
 }
 
 struct AvatarView: View {
     @Binding var showProfile: Bool
+    // This gives a typew of the user which allows us to show different Avatars
+    @EnvironmentObject var user: UserStore
     var body: some View {
-        Button(action: { self.showProfile.toggle()}) {
-            Image("Matthew")
-                .renderingMode(.original)
-                .resizable()
-                .frame(width: 36, height: 36)
-                .clipShape(Circle())
+        VStack {
+            if user.isLogged {
+                Button(action: { self.showProfile.toggle()}) {
+                Image("Matthew")
+                    .renderingMode(.original)
+                    .resizable()
+                    .frame(width: 36, height: 36)
+                    .clipShape(Circle())
+            }
+            } else {
+                Button(action: { self.user.showLogin.toggle()}) {
+                Image(systemName: "person")
+                    .foregroundColor(.primary)
+                    .font(.system(size: 16, weight: .medium))
+                    .frame(width: 36, height: 36)
+                    .background(Color("background3"))
+                    .clipShape(Circle())
+                    .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
+            }
+                
+            }
         }
     }
 }
