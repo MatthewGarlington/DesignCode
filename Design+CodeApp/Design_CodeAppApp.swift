93e4bcae8f589ad9d962955fb232f92927648271
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-17 09:30:08 -0500

    prototyping: Changed Launch to Courselist for testing

--- Design+CodeApp/Design_CodeAppApp.swift
+++ Design+CodeApp/Design_CodeAppApp.swift
@@ -31,7 +31,7 @@ struct Design_CodeAppApp: App {
    
     var body: some Scene {
         WindowGroup {
-            Home()
+            CourseList()
                 .environmentObject(UserStore())
             
         }
